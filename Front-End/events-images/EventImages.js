a235d3a701eab7761a27ca9f85497d855b360137
MuhammadMalook <maharmohammadmalook@gmail.com>
2022-10-01 17:17:13 -0700

    completed

--- Front-End/events-images/EventImages.js
+++ Front-End/events-images/EventImages.js
@@ -7,7 +7,11 @@ export const EventImages = {
    'tea party':require('../assets/events-images/tea.jpeg'),
    'dinner':require('../assets/events-images/dinner.jpg'),
    'sports':require('../assets/events-images/sports.png'),
-   'lunch':require('../assets/events-images/sports.png'),
-   'holi':require('../assets/events-images/sports.png'),
-   'shi':require('../assets/events-images/sports.png'),
+   'lunch':require('../assets/events-images/lunch.jpg'),
+   'breakfast':require('../assets/events-images/breakfast.jpg'),
+   'holi':require('../assets/events-images/holi.jpeg'), 
+   'session':require('../assets/events-images/session.png'),
+   'Workshop':require('../assets/events-images/workshop.jpeg'),
+   
+   
 }
